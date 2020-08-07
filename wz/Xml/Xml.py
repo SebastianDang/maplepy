2a7a4c0eba711c48046c1248732ccac93a609756
Sebastian Dang <s1dang@ucsd.edu>
2020-08-07 16:52:44 -0700

    Add comments for readability

--- wz/Xml/Xml.py
+++ wz/Xml/Xml.py
@@ -4,11 +4,19 @@ import xml.etree.ElementTree as ET
 
 
 class Layer(Enum):
+    """Enum used to parse xmls by type"""
     TAGS = 1
     CANVAS_ARRAY = 2
 
 
 class Xml:
+    """
+    Basic xml reader for elementary objects:
+        Back
+        Tile
+        Obj
+    """
+
     def __init__(self):
         self.root = None
         self.name = None
@@ -25,30 +33,52 @@ class Xml:
         self.root = ET.parse(file).getroot()
 
     def parse_root(self, layer):
+        """Parse the xml file beginning at root."""
+
         if not self.root:
             print('No file opened.')
             return
+
         try:
+
             info = {}
             objects = {}
+
             # Parse xml
             for child in self.root:
+
+                # A single list of values
                 if child.attrib.get('name') == 'info':
                     info = self.parse_info(child)
+
+                # Nested values
                 else:
+
+                    # Start at tags
                     if layer == Layer.TAGS:
                         objects[child.get('name')] = self.parse_tags(child)
+
+                    # Start at canvases
                     elif layer == Layer.CANVAS_ARRAY:
                         objects[child.attrib.get(
                             'name')] = self.parse_canvas_array(child)
+
             # Set variables
             self.name = self.root.get('name')
             self.info = info
             self.objects = objects
+
         except:
             print('Error while parsing.')
 
     def parse_info(self, info):
+        """
+        <imgdir name="info"/>
+            <int/>
+            <string/>
+        </imgdir>
+        """
+
         item = {}
         value_tags = ['int', 'string']
         for value in info:
@@ -57,33 +87,64 @@ class Xml:
         return item
 
     def parse_tags(self, tags):
+        """
+        <imgdir>
+            <imgdir/>
+            ...
+            <imgdir/>
+        </imgdir>
+        """
+
         items = {}
         for child in tags:
             items[child.get('name')] = self.parse_item_array(child)
         return items
 
     def parse_item_array(self, array):
+        """
+        <imgdir>
+            <imgdir/>
+            ...
+            <imgdir/>
+        </imgdir>
+        """
+
         items = []
         for child in array:
             items.append(self.parse_canvas_array(child))
         return items
 
     def parse_canvas_array(self, canvases):
+        """
+        <imgdir>
+            <canvas/>
+            ...
+            <canvas/>
+        </imgdir>
+        """
+
         items = []
         for child in canvases:
             items.append(self.parse_canvas(child))
         return items
 
     def parse_canvas(self, canvas):
+        """
+        <canvas>
+            <extended/>
+            <vector/>
+            <int/>
+            <string/>
+        </canvas>
+        """
+
         item = {}
         extended_tags = ['extended']
         vector_tags = ['vector']
         value_tags = ['int', 'string']
-        # Canvas values
         item['name'] = canvas.get('name')
         item['width'] = canvas.get('width')
         item['height'] = canvas.get('height')
-        # Inner items
         for value in canvas:
             if value.tag in extended_tags:
                 item['extended'] = self.parse_extended(value)
@@ -95,6 +156,14 @@ class Xml:
         return item
 
     def parse_extended(self, extended):
+        """
+        <extended>
+            <vector/>
+            ...
+            <vector/>
+        </extended>
+        """
+
         items = []
         vector_tags = ['vector']
         for value in extended:
