a279a396dd78ddf10d71ba79fc0b64ca55e74b4a
Sebastian Dang <s1dang@ucsd.edu>
2020-08-10 15:20:38 -0700

    Add image unit test

---
+++ test/test_nximage.py
@@ -0,0 +1,15 @@
+import os
+from nx.nxfile import NXFile
+from maplepy.nx.nxsprite import NXSprite
+
+path = 'P:/Downloads/Resources'  # TODO: Change this to use your nx path
+
+
+def test_nximage():
+    node = NXFile(os.path.join(path, 'map.nx')).resolve(
+        "Tile/grassySoil.img/bsc/0")
+    byte = node.getImage()
+    assert node.name == '0'
+    assert node.width == 90
+    assert node.height == 60
+    assert len(byte) == 90 * 60 * 4
