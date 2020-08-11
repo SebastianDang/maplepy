03e8e4f0ff2498983a4d7f81bb5a1ab67fefaf12
Sebastian Dang <s1dang@ucsd.edu>
2020-08-10 19:53:53 -0700

    Add second test for objects

--- test/test_nximage.py
+++ test/test_nximage.py
@@ -12,3 +12,13 @@ def test_nximage():
     assert node.width == 90
     assert node.height == 60
     assert len(byte) == 90 * 60 * 4
+
+
+def test_nximage2():
+    node = NXFile(os.path.join(path, 'map.nx')).resolve(
+        "Obj/acc1.img/grassySoil/nature/0/0")
+    byte = node.getImage()
+    assert node.name == '0'
+    assert node.width == 131
+    assert node.height == 39
+    assert len(byte) == 131 * 39 * 4
