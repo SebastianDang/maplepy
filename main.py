b2d03ca38d54e98b2b36cad74ab28994a7c4833e
Sebastian Dang <s1dang@ucsd.edu>
2021-02-11 22:17:44 -0800

    Remove path include

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
 import logging
-from maplepy.game.game import Game
 
-# Add nx to system path
-sys.path.insert(0, './nx')
+from maplepy.game.game import Game
 
 # Set up logging module
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s %(message)s',
